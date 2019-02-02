61f91042aab877cbda0dfd6c9732deae96131d64
floralyellow <cecilia.huang.pro@gmail.com>
2019-02-02 17:02:03 +0100

    checkout functionnality  and collection

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Form\CardType;
 use App\Model\Card;
+use App\Entity\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 /**
  * @Route("/checkout")
@@ -16,14 +18,20 @@ class CheckoutController extends AbstractController
     /**
      * @Route("/payment", name="checkout_payment", methods={"GET","POST"})
      */
-    public function payment(Request $request)
+    public function payment(Request $request, SessionInterface $session)
     {
         $card = new Card();
         $form = $this->createForm(CardType::class, $card);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // @todo process payment
+            $cartId = $session->get('cart');
+            $repositoryCart = $this->getDoctrine()->getRepository(Cart::class);
+            $cart = $repositoryCart->find($cartId);
+            foreach($cart->getCartProducts() as $cartProduct){
+                var_dump($cartProduct);
+            }
+            //header('Location:index');
         }
 
         return $this->render('checkout/payment.html.twig', [
