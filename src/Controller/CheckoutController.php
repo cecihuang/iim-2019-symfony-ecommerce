cc954bcc55916173538cfe5a7d67053cf1c61b58
floralyellow <cecilia.huang.pro@gmail.com>
2019-02-02 17:35:33 +0100

    orders functionnality, creating files

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Orders;
+use App\Entity\OrderProduct;
 use App\Form\CardType;
 use App\Model\Card;
 use App\Entity\Cart;
+use App\Entity\CartProduct;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,16 +23,30 @@ class CheckoutController extends AbstractController
      */
     public function payment(Request $request, SessionInterface $session)
     {
+        $objectManager = $this->getDoctrine()->getManager();
         $card = new Card();
         $form = $this->createForm(CardType::class, $card);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $order = new Orders();
+            $objectManager->persist($order);
+            $objectManager->flush();
             $cartId = $session->get('cart');
             $repositoryCart = $this->getDoctrine()->getRepository(Cart::class);
             $cart = $repositoryCart->find($cartId);
+            $repositoryCartProducts = $this->getDoctrine()->getRepository(CartProduct::class);
             foreach($cart->getCartProducts() as $cartProduct){
-                var_dump($cartProduct);
+                $orderProduct = new OrderProduct();
+                $orderProduct->setProductId($cartProduct->getProduct()->getId());
+                $orderProduct->setOrderId($order->getId());
+                $orderProduct->setQuantity($cartProduct->getQuantity());
+                $objectManager->persist($orderProduct);
+                $objectManager->flush();
+                $objectManager->remove($cartProduct);
+                $objectManager->persist($cart);
+                $objectManager->flush();
+
             }
             //header('Location:index');
         }
